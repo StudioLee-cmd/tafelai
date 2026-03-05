8062333d58349327c4e79942a71c1be218ddc876
StudioLee-cmd <tim@studiolee.nl>
2026-03-05 01:02:42 +0100

    Fix siteDetails: niche field, live siteUrl, siteName, language nl-NL; FreeTrialModal uses niche

--- src/data/siteDetails.ts
+++ src/data/siteDetails.ts
@@ -1,11 +1,12 @@
 export const siteDetails = {
     siteName: 'TafelAI',
-    siteUrl: 'https://tafelai.nl/',
+    niche: 'Restaurants',
+    siteUrl: 'https://www.tafelai.nl/',
     metadata: {
         title: 'TafelAI - AI Telefoniste & Automatisering voor Restaurants',
         description: 'TafelAI helpt restaurants groeien met AI. Van automatische reserveringen tot slimme reviews & marketing.',
     },
-    language: 'nl-nl',
+    language: 'nl-NL',
     locale: 'nl-NL',
     siteLogo: '/images/logo-transparent.png',
     googleAnalyticsId: '', // e.g. G-XXXXXXX,
